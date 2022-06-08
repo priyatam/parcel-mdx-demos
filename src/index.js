468ce5af9203c11106d685b63f78624e49f371a8
Priyatam <mudivarp@amazon.com>
2022-06-08 14:59:11 -0700

    added a few sample components

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import * as React from "react";
 import { render } from "react-dom";
 import Hello from './hello.mdx';
 
+import ReactStarsRating from 'react-awesome-stars-rating';
+
 export function add(a, b) {
-    return a + b;
+    return a + b
+}
+
+const onChange = (value) => {
+  console.log(`React Stars Rating value is ${value}`)
+}
+
+const ReactStarsExample = ({ value }) => {
+  return <ReactStarsRating onChange={onChange} value={value} />
 }
 
 render(
   <div>
-    test
     <Hello />
+    <ReactStarsExample />
   </div>,
   document.querySelector("#root")
-);
+)
